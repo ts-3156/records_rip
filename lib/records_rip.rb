6439c0d0957cd7c18cb6a2db218f97ab4a453496
ts-3156 <ts_3156@yahoo.co.jp>
2018-12-20 04:06:37 +0900

    Add require

--- lib/records_rip.rb
+++ lib/records_rip.rb
@@ -1,6 +1,7 @@
 require "active_support"
 require "records_rip/version"
 require "records_rip/model"
+require "records_rip/tomb"
 
 ActiveSupport.on_load(:active_record) do
   include(RecordsRip::Model)
